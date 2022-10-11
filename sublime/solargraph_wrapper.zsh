d80d053a48e345d391a964b70575b937025dc0b0
Brad Lindsay <sluggy.fan@gmail.com>
2022-10-11 13:46:13 -0500

    sublime: Default ruby versions for LSP Solargraph
    If a `.ruby-version` file doesn't exist, this should default to using
    the 3.0.3 version of Ruby for Solargaraph's LSP.

--- sublime/solargraph_wrapper.zsh
+++ sublime/solargraph_wrapper.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 # Pass in the path to the project or folder
-rb_version=$(head -n 1 $1/.ruby-version)
+rb_version=${$(head -n 1 $1/.ruby-version || echo -n '3.0.3')#ruby-}
 /Users/bladmin/code/devtools/ruby-$rb_version/bin/solargraph stdio
